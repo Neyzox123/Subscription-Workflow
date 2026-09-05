8b997075837dd4b84caffb25f063f484ab8483dd
Neyzox123 <neyzoxmailpro@gmail.com>
2026-09-05 18:16:51 +0000

    kk

--- models/Plan.js
+++ models/Plan.js
@@ -5,4 +5,13 @@ const findById = async (id) => {
         'SELECT * FROM plans WHERE id = ?',
         [id]
     )
+    return rows[0];
 };
+
+const findAll = async (all) => {
+    const [rows] = await pool.execute(
+        'SELECT * FROM plans'
+        [all]
+    )
+    return rows[0];
+}
